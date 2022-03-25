56286174d4b643841f5a5867e1df6f86beafb187
Anndrey Macedo <anndreyy@live.com>
2022-03-25 00:20:06 -0300

    Inclusão do servidor socket para comunicação com o cliente

--- configs/express.js
+++ configs/express.js
@@ -1,22 +1,43 @@
 const express = require('express')
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 const app = express()
-const port = 3000
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || '*'
+}));
+app.use(helmet());
+app.use(express.json());
+app.use(morgan('dev'));
 
 var route, routes = [];
 
 app.use('/', require('../api/routes/'));
 
-app._router.stack.forEach(function(middleware){
-    if(middleware.route){ // routes registered directly on the app
+app.post('/login', (req, res, next) => {
+    res.json({
+        token: '123456'
+    });
+});
+
+app._router.stack.forEach(function (middleware) {
+    if (middleware.route) { // routes registered directly on the app
         routes.push(middleware.route);
-    } else if(middleware.name === 'router'){ // router middleware 
-        middleware.handle.stack.forEach(function(handler){
+    } else if (middleware.name === 'router') { // router middleware 
+        middleware.handle.stack.forEach(function (handler) {
             route = handler.route;
             route && routes.push(route);
         });
     }
 });
 
-console.log(routes)
+// Show routes
+routes.map(route => {
+    for (const method in route.methods) {
+        console.log(method, route.path);
+    }
+})
 
 module.exports = app;
--- index.js
+++ index.js
@@ -3,30 +3,14 @@
  */
 
 
-// // Instancia o express
-// const express = require('./configs/express');
-// require('dotenv').config();
+require('dotenv').config();
 
-// express.listen(process.env.PORT, () => {
-//     console.log(`app listening on port ${process.env.PORT}!`);	
-//     }
-// );
+const express = require('./configs/express');
+const socket = require('./configs/socket');
 
+const server = express.listen(process.env.PORT, () => {
+    console.log(`app listening on port ${process.env.PORT}!`);	
+    }
+);
 
-const { App } = require("uWebSockets.js");
-const { Server } = require("socket.io");
-
-const app = new App();
-const io = new Server();
-
-io.attachApp(app);
-
-io.on("connection", (socket) => {
-  // ...
-});
-
-app.listen(3000, (token) => {
-  if (!token) {
-    console.warn("port already in use");
-  }
-});
+socket(server);
